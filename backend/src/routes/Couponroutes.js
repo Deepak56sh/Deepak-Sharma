99face160732436e5fb253febbab556b99d90af2
Deepak Sharma <deepaks.cnc@gmail.com>
2026-08-11 14:58:49 +0530

    ,

--- backend/src/routes/Couponroutes.js
+++ backend/src/routes/Couponroutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { validateCoupon, getMyCoupons } = require('../controllers/couponController');
+const { validateCoupon, getMyCoupons } = require('../controllers/Couponcontroller');
 const customerAuth = require('../middleware/customerAuth');
 
 router.post('/validate', validateCoupon);
