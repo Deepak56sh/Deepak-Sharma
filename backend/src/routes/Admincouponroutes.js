a50ab8bbebadc3a66a2077ceaa8c131a38171790
Deepak Sharma <deepaks.cnc@gmail.com>
2026-08-11 15:19:34 +0530

    ,

--- backend/src/routes/Admincouponroutes.js
+++ backend/src/routes/Admincouponroutes.js
@@ -6,7 +6,7 @@ const {
   updateCoupon,
   toggleCoupon,
   deleteCoupon,
-} = require('../controllers/adminCouponController');
+} = require('../controllers/Admincouponcontroller');
 
 
 const { protect } = require('../middleware/auth');
